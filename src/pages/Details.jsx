7dc5110606145a3e54ea563d8d64b70575666c62
Hasib Hossain <boraborhasib@gmail.com>
2023-11-08 21:52:20 +0600

    updated

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { BsFillBookmarkFill, BsJournalRichtext } from 'react-icons/bs';
+import { GiNetworkBars } from 'react-icons/gi';
+import { BiStopwatch } from 'react-icons/bi';
+import { PiCertificateDuotone } from 'react-icons/pi';
 
 const Details = () => {
     const { id } = useParams();
@@ -14,17 +18,48 @@ const Details = () => {
         }
         fetchData();
     }, [])
-    const fixData = data[0]?.allCourses?.find((course) => course.id === id);
-    console.log(fixData)
+    const { category, title, level, details } = data[0]?.allCourses?.find((course) => course.id === id) || {};
 
 
-    return loading ? <div className="">
+    return loading ? <div className="flex justify-center items-center h-screen">
         <h1>Loading...</h1>
     </div> : (
-        <div>
-            <h1>About : {fixData.title}</h1>
+        <section>
+            <div className="h-[300px] w-full bg-[#121d42] text-[#f9fafb]">
+                <div className="w-[90%] relative py-8  mx-auto">
+                    <div className="flex items-center gap-4">
+                        <h1>{category}</h1>
+                        <div className='border-r border-white h-[20px] '> </div>
+                        <BsFillBookmarkFill className="text-xl" />
+                    </div>
+                    <div className="mb-10">
 
-        </div>
+                        <h1 className='text-5xl  font-bold  mt-3'>{title}</h1>
+                    </div>
+                    <div className="flex items-center space-x-6">
+                        <div className="flex items-center ">
+                            <GiNetworkBars className="text-base" />
+                            <span className="ml-1">{level}</span>
+                        </div>
+                        <div className="flex items-center ">
+                            <BsJournalRichtext className="text-base" />
+                            <span className=" ml-1">{details.videos} Lessons</span>
+                        </div>
+                        <div className="flex items-center ">
+                            <BiStopwatch className="text-xl" />
+                            <span className=" ml-1">{details.length}h</span>
+                        </div>
+                        <div className="flex items-center ">
+                            <PiCertificateDuotone className="text-xl" />
+                            <span className=" ml-1">Certificate of Completion</span>
+                        </div>
+                    </div>
+                    <div className="fixed top-[300px] bg-blue w-[250px] h-32 right-0">
+
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
