8c4fbb9cd1261e5702944085048539d7f656d6db
Hasib Hossain <boraborhasib@gmail.com>
2023-11-08 18:58:20 +0600

    updated

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -22,15 +22,15 @@ const Navbar = () => {
         }
     ]
     return (
-        <nav className='px-8 py-2 flex items-center'>
-            <div className="flex item-center">
+        <nav className='px-8 py-2 flex justify-between items-center'>
+            <div className="flex item-center gap-8">
                 <div className="">
                     <div className="">
                         <h1 className='text-xl font-bold '>educative</h1>
                     </div>
                 </div>
                 <div className="">
-                    <ul className='flex space-x-4'>
+                    <ul className='flex items-center  space-x-4'>
                         {navItems.map((item, index) => (
                             <li key={index} className='text-base font-medium text-gray-500 hover:text-gray-900'>
                                 <a href={item.link}>{item.title}</a>
@@ -41,8 +41,17 @@ const Navbar = () => {
             </div>
             <div className="">
                 <div className="">
-                    <div className="">
-                        <CiSearch className="w-6 h-6" />
+                    <div className="flex justify-center items-center gap-3">
+                        <button className='flex px-2 py-3 hover:bg-gray-100 duration-200 items-center gap-3 '>
+                            <CiSearch className="w-6 h-6" />
+                            <span className=' font-bold'>Search</span>
+                        </button>
+                        <button className='px-3 hover:bg-gray-100 duration-200 py-3'>
+                            <span className='font-bold'>Login</span>
+                        </button>
+                        <button className='bg-[#5553ff] text-white font-bold px-3 py-2 rounded'>
+                            <span>Join for free</span>
+                        </button>
                     </div>
                 </div>
             </div>
